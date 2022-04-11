d0d6a9f1b88328a6146ba2c5d494452766636184
TellexC <103390990+TellexC@users.noreply.github.com>
2022-04-10 19:15:29 -0700

    remove bulk #1

--- data/imminent/BendyDeath.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-    setPropertyFromClass('GameOverSubstate', 'characterName', 'bendydeath')
-    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'Bendy/bendyInitialDeath')
-    setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Bendy/bendydeath')
-    setPropertyFromClass('GameOverSubstate', 'endSoundName', 'Bendy/bendydeathConfirm')
-end
