b0d5fe48e1ec8bfa12cd734c5a1739dd24e1daed
TellexC <103390990+TellexC@users.noreply.github.com>
2022-04-10 19:15:40 -0700

    remove bulk #2

--- data/imminent/cutscene.lua
+++
@@ -1,9 +0,0 @@
-local allowCountdown = false
-function onStartCountdown()
-	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('Bendy/imminent');
-		allowCountdown = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
-end
